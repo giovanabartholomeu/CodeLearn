674d24c32ff16cb82dccfd45aac9c78085837525
giovanabarth <148559217+giovanabarth@users.noreply.github.com>
2025-05-14 10:35:48 -0300

    commit initial

---
+++ CodeLearn/src/test/java/com/giovana/codelearn/CodeLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovana.codelearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
